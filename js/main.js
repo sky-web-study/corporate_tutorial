045c9daea394aa90e748be19f1cd7180a48b2be4
DESKTOP-59SUQN4\SekiyaN <topaz_11th@yahoo.co.jp>
2020-10-22 16:51:48 +0900

    12th day commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 $(function () {
+
   const $win = $(window);
-  const $btnMenu = $('.btn-menu');
-  const $nav = $('header > nav');
-  const isAnimate  = 'is-animate';
-  const isOpen = 'is-open';
 
-  //ボタンクリック時の処理
-  $btnMenu.on('click', function () {
+  //スマホメニュー
+  (function () {
+    const $btnMenu = $('.btn-menu');
+    const $nav = $('header > nav');
+    const isAnimate  = 'is-animate';
+    const isOpen = 'is-open';
 
+    //ボタンクリック時の処理
+    $btnMenu.on('click', function () {
       const $this = $(this);
       if($nav.is(':animated')){
          return;
       }
-
       if(!$this.hasClass(isOpen)){
          $nav.slideDown();
          $this.addClass(isOpen).addClass(isAnimate);
@@ -20,18 +22,36 @@ $(function () {
          $nav.slideUp();
          $this.removeClass(isOpen).removeClass(isAnimate);
       }
-  });
+    });
 
-  //ウィンドウリサイズ時の処理
-  $win.on('resize', function () {
-
-   if ($win.width() > 768) {
-      $nav.show();
-      $btnMenu.removeClass(isOpen).removeClass(isAnimate);
-    } else {
-      if (!$btnMenu.hasClass(isOpen)) {
-        $nav.hide();
+    //ウィンドウリサイズ時の処理
+    $win.on('resize', function () {
+      if ($win.width() > 768) {
+        $nav.show();
+        $btnMenu.removeClass(isOpen).removeClass(isAnimate);
+      } else {
+        if (!$btnMenu.hasClass(isOpen)) {
+          $nav.hide();
+        }
       }
-    }
-  });
- });
+    });
+  })();
+
+  // 見出しのアニメーション
+  (function () {
+    $win.on('scroll', function () {
+      const $heading = $('.mask_inner');
+      const isAnimate = 'is-animate';
+      $heading.each(function () {
+        const $this = $(this);
+        const headingTop = $this.offset().top;
+        const scrollPos = $win.scrollTop();
+        const wh = $win.height();
+        if(scrollPos > headingTop - wh + (wh / 4)){
+          $this.addClass(isAnimate);
+        }
+      });
+    });
+  })();
+
+});
